02f7980db084c69e3ce53c0408557721e0b54d11
Romain Warnan <romain.warnan@insee.fr>
2017-06-19 12:53:43 +0200

    Fin du des test de Reactor avec JUnit

--- src/main/java/fr/plaisance/reactor/ReactorSnippets.java
+++ src/main/java/fr/plaisance/reactor/ReactorSnippets.java
@@ -3,9 +3,13 @@ package fr.plaisance.reactor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+
+import static java.lang.System.out;
 
 public class ReactorSnippets {
 
@@ -22,20 +26,20 @@ public class ReactorSnippets {
     );
 
     @AfterEach
-    void afterEach(){
+    void afterEach() {
         System.out.println();
     }
 
     @Test
     void just() {
         Flux<String> fewWords = Flux.just("Hello", "World");
-        fewWords.subscribe(System.out::println);
+        fewWords.subscribe(out::println);
     }
 
     @Test
     void fromIterable() {
         Flux<String> manyWords = Flux.fromIterable(words);
-        manyWords.subscribe(System.out::println);
+        manyWords.subscribe(out::println);
     }
 
     @Test
@@ -49,6 +53,49 @@ public class ReactorSnippets {
                         Flux.range(1, 100),
                         (string, count) -> String.format("%2d. %s", count, string));
 
-        manyLetters.subscribe(System.out::println);
+        manyLetters.subscribe(out::println);
+    }
+
+    @Test
+    void restoringMissingLetters() {
+        Mono<String> missing = Mono.just("s");
+        Flux<String> allLetters = Flux
+                .fromIterable(words)
+                .flatMap(word -> Flux.fromArray(word.split("")))
+                .concatWith(missing)
+                .distinct()
+                .sort()
+                .zipWith(
+                        Flux.range(1, 100),
+                        (letter, index) -> String.format("%2d. %s", index, letter));
+        allLetters.subscribe(out::println);
+    }
+
+    @Test
+    void shortCircuit() {
+        Flux<String> helloPausedWorld = Mono.just("Hello")
+                .concatWith(Mono.just("World")
+                                .delaySubscription(Duration.ofSeconds(1)));
+        helloPausedWorld.subscribe(out::println);
+    }
+
+    @Test
+    void blocks() {
+        Flux<String> helloPausedWorld = Mono.just("Hello")
+                .concatWith(Mono.just("World")
+                                .delaySubscription(Duration.ofSeconds(1)));
+        helloPausedWorld.toStream().forEach(out::println);
+    }
+
+    @Test
+    void firstEmitting() {
+        Mono<String> mono = Mono.just("Ooops I'm late")
+                                .delaySubscription(Duration.ofMillis(100));
+        Flux<String> flux = Flux.just("Let's get", "the party", "started !")
+                                .delayElements(Duration.ofMillis(50));
+
+        Flux.firstEmitting(mono, flux)
+            .toIterable()
+            .forEach(out::println);
     }
 }
